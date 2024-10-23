181ce271d8caa3e5ad3aac66b9f870271e02703d
Yg070212 <winter070212@gmail.com>
2024-10-23 21:45:46 +0900

    24-10-23 <가상 함수>

---
+++ program/Vultuer.cpp
@@ -0,0 +1,12 @@
+#include "Vultuer.h"
+
+Vultuer::Vultuer()
+{
+	attack = 20;
+	defense = 0;
+}
+
+void Vultuer::Move()
+{
+	cout << "Vulture Move" << endl;
+}
---
+++ program/Vultuer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Terran.h"
+
+class Vultuer :
+    public Terran
+{
+public:
+    Vultuer();
+
+    virtual void Move();
+};
+
