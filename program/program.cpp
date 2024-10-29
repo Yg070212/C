4f31894fd5acaa812429baefe784a0b465211ed0
Yg070212 <winter070212@gmail.com>
2024-10-29 21:45:34 +0900

    24-10-29 <템플릿>

--- program/program.cpp
+++ program/program.cpp
@@ -18,8 +18,32 @@ public:
 #pragma endregion
 	}
 
-	int & X() { return x; }
-	int & Y() { return y; }
+	int& X() { return x; }
+	int& Y() { return y; }
+
+	Vector2& operator + (const Vector2& clone)
+	{
+		Vector2 vector(this->x + clone.x, this->y + clone.y);
+
+		return vector;
+	}
+	Vector2& operator ++ ()
+	{
+		this->x++;
+		this->y++;
+
+		return *this;
+	}
+
+	Vector2& operator ++ (int value)
+	{
+		Vector2 clone(this->x, this->y);
+
+		this->x += 1;
+		this->y += 1;
+
+		return clone;
+	}
 };
 
 int main()
@@ -43,8 +67,12 @@ int main()
 	Vector2 vector1(1, 1);
 	Vector2 vector2(3, 3);
 
-	// Vector2 position = vector1 + vector2;
+	Vector2 position = vector1 + vector2;
 
+	cout << ++position.X() << endl;
+
+	cout << "Position X의 값 : " << position.X() << endl;
+	cout << "Position Y의 값 : " << position.Y() << endl;
 #pragma endregion
 
 
